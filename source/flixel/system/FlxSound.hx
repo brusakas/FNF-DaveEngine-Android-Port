b129d8eb8a99c98d2edf4da1cc4431f0f28635a5
brusakas <121342424+brusakas@users.noreply.github.com>
2024-05-18 22:27:56 +0100

    comment

--- source/flixel/system/FlxSound.hx
+++ source/flixel/system/FlxSound.hx
@@ -590,20 +590,20 @@ class FlxSound extends FlxBasic
 		_transform.volume = #if FLX_SOUND_SYSTEM (FlxG.sound.muted ? 0 : 1) * FlxG.sound.volume * #end
 			(group != null ? group.volume : 1) * _volume * _volumeAdjust;
 
-		if (_channel != null)
-		{
-			_channel.soundTransform = _transform;
-			@:privateAccess
-			if (_channel.__source != null)
+		//if (_channel != null)
+		//{
+			//_channel.soundTransform = _transform;
+			//@:privateAccess
+			//if (_channel.__source != null)
 			{
-				#if cpp
-				@:privateAccess
-				this._channel.__source.__backend.setPitch(_pitch);
+			//	//#if cpp
+				//@:privateAccess
+				// this._channel.__source.__backend.setPitch(_pitch);
 				// trace('changing $name pitch new $_pitch');
-				#end
-			}
-		}
-	}
+				//#end
+			//}/
+		//}
+	//}
 
 	/**
 	 * An internal helper function used to attempt to start playing
